3a6f4df4c560a85e4a59f84ba83e3d90a92564a8
Samhitha Tarra <samhitht@uci.edu>
2022-10-07 09:00:58 -0700

    part 1 complete, csrf

---
+++ sqli.py
@@ -0,0 +1,33 @@
+from requests import codes, Session
+
+LOGIN_FORM_URL = "http://localhost:8080/login"
+PAY_FORM_URL = "http://localhost:8080/pay"
+
+def submit_login_form(sess, username, password):
+    response = sess.post(LOGIN_FORM_URL,
+                         data={
+                             "username": username,
+                             "password": password,
+                             "login": "Login",
+                         })
+    return response.status_code == codes.ok
+
+def submit_pay_form(sess, recipient, amount):
+    # You may need to include CSRF token from Exercise 1.5 in the POST request below 
+    response = sess.post(PAY_FORM_URL,
+                    data={
+                        "recipient": recipient,
+                        "amount": amount,
+                    })
+    return response.status_code == codes.ok
+
+def sqli_attack(username):
+    sess = Session()
+    assert(submit_login_form(sess, "attacker", "attacker"))
+    pass
+
+def main():
+    sqli_attack("admin")
+
+if __name__ == "__main__":
+    main()
